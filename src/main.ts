2530fddf51075c0c16140135d1732dfe2f227848
Diego Alejandro Munévar Pérez <d.munevar@uniandes.edu.co>
2026-03-26 19:37:22 -0500

    swagger, class-validator-transformer y axios

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Skin4All Docs')
+    .setDescription('The Skin4All API description')
+    .setVersion('1.0')
+    .addTag('Skin4All')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  app.useGlobalPipes( new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
